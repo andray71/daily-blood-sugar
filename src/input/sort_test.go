77e4dc27f50c284a83ebaee373798a1b737769b7
Andray Siletsky <andray.siletsky@gmail.com>
2017-06-17 14:13:24 -0700

    updated sort_test

--- src/input/sort_test.go
+++ src/input/sort_test.go
@@ -14,12 +14,32 @@ func TestSort(t *testing.T) {
 		NewExercise(1,time.Now().Add(time.Hour)),
 	}
 
+	newEvents := make([]Event,len(events))
+	for i,e := range events {
+		println(e.GetTime().String())
+		newEvents[i] = events[i]
+	}
+	println("sorting")
+
+	Sort(newEvents)
 	for _,e := range events {
 		println(e.GetTime().String())
 	}
-	println("sorting")
-	Sort(events)
-	for _,e := range events {
+	println("sorted")
+
+	for _,e := range newEvents {
 		println(e.GetTime().String())
+	}
+	if(!events[0].GetTime().Equal(newEvents[2].GetTime())){
+		t.Fatal("not match")
+	}
+	if(!events[1].GetTime().Equal(newEvents[3].GetTime())){
+		t.Fatal("not match")
+	}
+	if(!events[2].GetTime().Equal(newEvents[1].GetTime())){
+		t.Fatal("not match")
+	}
+	if(!events[3].GetTime().Equal(newEvents[0].GetTime())){
+		t.Fatal("not match")
 	}
 }
